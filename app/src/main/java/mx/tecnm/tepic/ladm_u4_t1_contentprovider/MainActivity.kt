30b159c4529d6312e1358a9709063b4519665b0c
Marco Antonio Ramirez Sandoval <60723739+Asouny@users.noreply.github.com>
2021-01-23 21:05:39 -0700

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u4_t1_contentprovider/MainActivity.kt
@@ -0,0 +1,26 @@
+package mx.tecnm.tepic.ladm_u4_t1_contentprovider
+
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CallLog
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        }
+
+    }
+
+
+
+}
